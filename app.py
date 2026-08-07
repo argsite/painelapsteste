844b75c06fe5eea384835e88b1db21a95fca84c3
Miro Teixeira <78441204+argsite@users.noreply.github.com>
2026-08-07 10:55:34 -0300

    Change color mapping for 'suficiente' classification

--- app.py
+++ app.py
@@ -1632,7 +1632,7 @@ def render_score_dashboard(df: pd.DataFrame, spec: IndicatorSpec):
             color_discrete_map={
                 TXT["otimo"]: "#3CB371",       # verde
                 TXT["bom"]: "#1E90FF",         # azul
-                TXT["suficiente"]: "#9932CC",  # roxo
+                TXT["suficiente"]: "#DAA520",  # bege
                 TXT["regular"]: "#D2691E",     # laranja
             },
         )
